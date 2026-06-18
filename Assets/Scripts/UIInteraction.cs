c309be517fc28dae90c15f6eaf9045624910c20d
Catalin Catta <catalincatta@yahoo.com>
2026-06-18 23:26:41 +0300

    added create prices page (placeholder)

---
+++ Assets/Scripts/UIInteraction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+internal class UIInteraction : MonoBehaviour
+{
+    public GameObject menuTransform;
+    public GameObject createPricemenu1;
+    public GameObject createPricemenu2;
+    public GameObject editPrice;
+    public GameObject addPrice;
+
+    public void GoToCreatePricesPage()
+    {
+        menuTransform.SetActive(false);
+        createPricemenu1.SetActive(true);
+    }
+
+    public void GoToEditProductPage()
+    {
+        menuTransform.SetActive(false);
+        editPrice.SetActive(true);
+    }
+
+    public void GoToAddProductPage()
+    {
+        menuTransform.SetActive(false);
+        addPrice.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        createPricemenu1.SetActive(false);
+        //createPricemenu2.SetActive(false);
+        //editPrice.SetActive(false);
+        //addPrice.SetActive(false);
+        menuTransform.SetActive(true);
+    }
+}
